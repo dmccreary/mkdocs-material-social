051050932a7a850a0131fe5d5b6825c118207ad7
Dan Mccreary <Dan.McCreary@gmail.com>
2023-12-07 11:08:15 -0600

    Deployed dce3358 with MkDocs version: 1.5.3

---
+++ sims/template/template.js
@@ -0,0 +1,12 @@
+let canvasHeight = 400;
+let aspectRatio = 1.91; // Open Graph standard
+let canvasWidth = canvasHeight * aspectRatio;
+let radius = 200;
+function setup() {
+const canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent('canvas-container');
+  fill(245);
+  rect(0,0, canvasWidth, canvasHeight);
+  fill('green');
+  circle(canvasWidth/2, canvasHeight/2, radius);
+}
